0d300816d027c3fb20087fe5a34bd6402837c03e
Sharp Thinker <133127411+Sharp-Thinker@users.noreply.github.com>
2023-05-17 14:46:20 +0600

    Add files via upload

---
+++ Vector_sort.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> element(n);
+    cout << element.size();
+    // for (int i = 0; i < element.size(); i++)
+    // {
+    //     int x;
+    //     cin >> x;
+    //     element.push_back(x);
+    // }
+    // sort(element.begin(), element.end());
+    // for (int i = 0; i < element.size(); i++)
+    // {
+    //     cout << element[i] << " ";
+    // }
+
+    return 0;
+}
