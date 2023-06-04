964fa6a44c7b1578f1c7cce238a3324063c3e11e
Sharp Thinker <133127411+Sharp-Thinker@users.noreply.github.com>
2023-06-05 00:20:32 +0600

    Update Vector_sort.cpp

--- Vector_sort.cpp
+++ Vector_sort.cpp
@@ -10,18 +10,17 @@ int main()
     int n;
     cin >> n;
     vector<int> element(n);
-    cout << element.size();
-    // for (int i = 0; i < element.size(); i++)
-    // {
-    //     int x;
-    //     cin >> x;
-    //     element.push_back(x);
-    // }
-    // sort(element.begin(), element.end());
-    // for (int i = 0; i < element.size(); i++)
-    // {
-    //     cout << element[i] << " ";
-    // }
+    for (int i = 0; i < element.size(); i++)
+    {
+        int x;
+        cin >> x;
+        element.push_back(x);
+    }
+    sort(element.begin(), element.end());
+    for (int i = 0; i < element.size(); i++)
+    {
+        cout << element[i] << " ";
+    }
 
     return 0;
 }
