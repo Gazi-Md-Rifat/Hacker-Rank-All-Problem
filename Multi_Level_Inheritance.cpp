4afa986dfcbab71fb7f48bd97d1f164f9b3cb58c
Gazi Md. Rifat <133127411+Sharp-Thinker@users.noreply.github.com>
2023-08-10 20:26:12 +0600

    Add files via upload

---
+++ Multi_Level_Inheritance.cpp
@@ -0,0 +1,11 @@
+Hacker Rank Question Link : https://www.hackerrank.com/challenges/multi-level-inheritance-cpp/problem?isFullScreen=true
+
+// Multilevel Inheritance
+class Equilateral : public Isosceles
+{
+    public:
+        void equilateral()
+        {
+            cout << "I am an equilateral triangle" << endl;
+        }
+};
